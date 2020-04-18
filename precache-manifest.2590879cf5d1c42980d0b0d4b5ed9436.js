f7e8e3fb542c94f800f35198925772180ec3b093
deepakr1618 <deepak.r1618@gmail.com>
2020-04-18 19:02:43 +0530

    Updates

---
+++ precache-manifest.2590879cf5d1c42980d0b0d4b5ed9436.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a5a30a074f332ae6c1dd4d7206e285",
+    "url": "/E-Comm-React/index.html"
+  },
+  {
+    "revision": "0c37be80e35c67442b42",
+    "url": "/E-Comm-React/static/css/main.ad53a8de.chunk.css"
+  },
+  {
+    "revision": "60c70a9d0378a8ac5b33",
+    "url": "/E-Comm-React/static/js/2.c9dd92fa.chunk.js"
+  },
+  {
+    "revision": "58b39d2e087df2618528b7ec2132c7f3",
+    "url": "/E-Comm-React/static/js/2.c9dd92fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c37be80e35c67442b42",
+    "url": "/E-Comm-React/static/js/main.22626421.chunk.js"
+  },
+  {
+    "revision": "da1c7c89cf7deb21502b",
+    "url": "/E-Comm-React/static/js/runtime-main.a7799ad8.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/E-Comm-React/static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/E-Comm-React/static/media/original.ed28accb.svg"
+  }
+]);
