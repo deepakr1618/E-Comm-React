eaab1ce92a22f6cbe6b212b102b32948469e4c37
deepakr1618 <deepak.r1618@gmail.com>
2020-04-18 18:55:27 +0530

    Production build

---
+++ build/precache-manifest.d7c7d5a1d2c4e26651e0eacb09e83d30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f61cec19028ee99195da7bacd9463dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c37be80e35c67442b42",
+    "url": "/static/css/main.ad53a8de.chunk.css"
+  },
+  {
+    "revision": "11eb48be0955de3eba9c",
+    "url": "/static/js/2.d978a662.chunk.js"
+  },
+  {
+    "revision": "58b39d2e087df2618528b7ec2132c7f3",
+    "url": "/static/js/2.d978a662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c37be80e35c67442b42",
+    "url": "/static/js/main.22626421.chunk.js"
+  },
+  {
+    "revision": "923b6ab2a1bf496bf88d",
+    "url": "/static/js/runtime-main.1886e776.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/original.ed28accb.svg"
+  }
+]);
